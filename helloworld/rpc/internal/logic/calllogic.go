3edfd7ffd020369a1bbae7ed3336bf414354894c
linzhengen <linzhengen@yahoo.co.jp>
2022-10-22 06:20:36 +0900

    init hello world

---
+++ helloworld/rpc/internal/logic/calllogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"helloworld/rpc/helloworld"
+	"helloworld/rpc/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CallLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewCallLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CallLogic {
+	return &CallLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *CallLogic) Call(in *helloworld.Request) (*helloworld.Response, error) {
+	// todo: add your logic here and delete this line
+
+	return &helloworld.Response{}, nil
+}
