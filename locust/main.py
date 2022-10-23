ed870ea94108d60e4c7436712d8cac8e83d85507
linzhengen <linzhengen@yahoo.co.jp>
2022-10-23 18:57:53 +0900

    add locust

---
+++ locust/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from locust import HttpUser, task, between
+
+class WebsiteUser(HttpUser):
+    host = "http://api-svc.api.svc.cluster.local:8888"
+    wait_time = between(1, 2)
+
+    @task
+    def get_hello_world(self):
+        self.client.get("/helloWorld")
