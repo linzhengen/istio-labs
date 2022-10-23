caf0336b5b9af3f4095642d785b303830b75c91d
linzhengen <linzhengen@yahoo.co.jp>
2022-10-23 19:59:39 +0900

    update rpc listen

--- Makefile
+++ Makefile
@@ -10,5 +10,5 @@ build:
 	skaffold build --file-output output.json
 
 load-test:
-	echo 'GET http://localhost:8888/helloWorld' | vegeta attack -rate=30 -duration=120s | vegeta report
+	echo 'GET http://localhost:8888/sayHello' | vegeta attack -rate=30 -duration=120s | vegeta report
 
--- locust/main.py
+++ locust/main.py
@@ -3,9 +3,9 @@
 from locust import HttpUser, task, between
 
 class WebsiteUser(HttpUser):
-    host = "http://api-svc.api.svc.cluster.local:8888"
+    host = 'http://api-svc.api.svc.cluster.local:8888'
     wait_time = between(1, 2)
 
     @task
     def get_hello_world(self):
-        self.client.get("/helloWorld")
+        self.client.get('/sayHello')
